338e0f24de0a1142fd0aa319c4c5778938c8008b
sokmunki <sokmunki@gmail.com>
2016-03-27 13:25:23 -0700

    Started Class Diagram
    As this is a more complicated project, I've started a class diagram to
    map out what is needed for the end result. In the interim, the project
    will be locally based and stand alone. The class diagram is intended to
    lay out the end goal of a web-based, multi-user, learning engine that is
    secure, interactive, and provides useful data analytics.

--- LearningEngine/LE_TestDriver/Program.cs
+++ LearningEngine/LE_TestDriver/Program.cs
@@ -10,6 +10,21 @@ namespace LE_TestDriver
     {
         static void Main(string[] args)
         {
+            // Login process - credentials need to be encrypted and separate
+
+            /* Search for personal record / create new one
+             * Upload to SQL server or encrypt and store locally
+             */
+
+            // Select study file - *.xml
+
+            /* Record session time, total number of questions, total correct, percent, each
+             * individual question right/wrong
+             * 
+             * Upload to SQL server or encrypt and store locally for future use or future
+             * uploading
+             */
+
         }
     }
 }
